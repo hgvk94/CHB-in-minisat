6e184a42ff2316c19fef678dbee0dc010ae52af6
hgvk <hgvk94@gmail.com>
2016-03-06 01:38:25 +0530

    Initial Commit

---
+++ core/depend.mk
@@ -0,0 +1,23 @@
+/home/hari/Documents/CHB/core/Main.o /home/hari/Documents/CHB/core/Main.or /home/hari/Documents/CHB/core/Main.od /home/hari/Documents/CHB/core/Main.op: /home/hari/Documents/CHB/core/Main.cc \
+ /home/hari/Documents/CHB/utils/System.h \
+ /home/hari/Documents/CHB/mtl/IntTypes.h \
+ /home/hari/Documents/CHB/utils/ParseUtils.h \
+ /home/hari/Documents/CHB/utils/Options.h \
+ /home/hari/Documents/CHB/mtl/Vec.h /home/hari/Documents/CHB/mtl/XAlloc.h \
+ /home/hari/Documents/CHB/core/Dimacs.h \
+ /home/hari/Documents/CHB/core/SolverTypes.h \
+ /home/hari/Documents/CHB/mtl/Alg.h /home/hari/Documents/CHB/mtl/Map.h \
+ /home/hari/Documents/CHB/mtl/Alloc.h \
+ /home/hari/Documents/CHB/core/Solver.h \
+ /home/hari/Documents/CHB/mtl/Heap.h
+/home/hari/Documents/CHB/core/Solver.o /home/hari/Documents/CHB/core/Solver.or /home/hari/Documents/CHB/core/Solver.od /home/hari/Documents/CHB/core/Solver.op: /home/hari/Documents/CHB/core/Solver.cc \
+ /home/hari/Documents/CHB/utils/System.h \
+ /home/hari/Documents/CHB/mtl/IntTypes.h \
+ /home/hari/Documents/CHB/mtl/Sort.h /home/hari/Documents/CHB/mtl/Vec.h \
+ /home/hari/Documents/CHB/mtl/XAlloc.h \
+ /home/hari/Documents/CHB/core/Solver.h \
+ /home/hari/Documents/CHB/mtl/Heap.h /home/hari/Documents/CHB/mtl/Alg.h \
+ /home/hari/Documents/CHB/utils/Options.h \
+ /home/hari/Documents/CHB/utils/ParseUtils.h \
+ /home/hari/Documents/CHB/core/SolverTypes.h \
+ /home/hari/Documents/CHB/mtl/Map.h /home/hari/Documents/CHB/mtl/Alloc.h
